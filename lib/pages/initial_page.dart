5eba09875b76b744d0fc4705c87b73067a81eb09
Jean Lucas <jeanl.camillo@gmail.com>
2019-09-24 22:20:25 -0300

    Incluído o analysis_options

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class InitialPage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: Container(
         child: Center(
           child: Text(
